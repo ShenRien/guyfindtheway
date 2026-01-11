595a1e3cbde3f1170d1ed1714f2569dc295ed353
shenrien <shenrien@github.com>
2026-01-12 06:16:01 +0700

    Ddoi font chu toan cuc blog - eleventy.config - change public font locate

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "Guy find the way",
-  shortTitle: "Cuộc tìm kiếm sao biển 💊",
+  shortTitle: "Cuộc tìm kiếm sao biển",// 💊🤪\u{1F48A}",
+  hasPill: true, // Đặt một biến đánh dấu
   description:
     "A beautiful and elegant blog theme starter project por Eleventy",
   url: "https://shenrien.github.io/guyfindtheway/",
